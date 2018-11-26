aac6d5b350a74caa742678f3d2487de495bbe461
Perry Brandiezs <pbrandiezs@gmail.com>
2018-11-26 15:10:16 -0800

    Added IO logic to reverse-file.py

--- reverse-file.py
+++ reverse-file.py
@@ -7,4 +7,13 @@ parser.add_argument('--limit', '-l', type=int, help='the number of lines to read
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
-print(args)
+
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+
+    for line in lines:
+        print(line.strip()[::-1])
