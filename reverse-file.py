1eb3e39da16c72ba796b3e3d9c3b35234535d3ba
Perry Brandiezs <pbrandiezs@gmail.com>
2018-11-27 09:58:21 -0800

    Added exception handling to reverse-file.py

--- reverse-file.py
+++ reverse-file.py
@@ -4,16 +4,22 @@ import argparse
 parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
 parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
-parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s verison 1.0')
 
 args = parser.parse_args()
 
-with open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse()
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
 
-    if args.limit:
-        lines = lines[:args.limit]
+        if limit:
+            lines = lines[:limit]
 
-    for line in lines:
-        print(line.strip()[::-1])
+        for line in lines:
+            print(line.strip()[::-1])
